dcf0ed7dce2dc291141b90cb716f23dc67e6562d
vipulsri123 <incredible.vipul2478@gmail.com>
2018-06-14 11:42:32 +0530

    added

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class calculation
+    {
+        public Int32 sum(Int32 a, Int32 b)
+        {
+            return a + b;
+        }
+        public Int32 sub(Int32 a, Int32 b)
+        {
+            return a - b;
+        }
+        public Int32 mul(Int32 a, Int32 b)
+        {
+            return a * b;
+        }
+        public Int32 div(Int32 a, Int32 b)
+        {
+            return a / b;
+        }
+    }
+}
